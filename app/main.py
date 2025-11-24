243195d539b9dfebd825875a60975d14deba7fa0
unknown <rajunaidu4a3@gmail.com>
2025-11-24 18:26:21 +0530

    print msg changed

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello from your Python API with Docker & CI/CD!"}
+    return {"message": "Hello krupa am from your Python API with Docker & CI/CD!"}
 
 @app.get("/add/{a}/{b}")
 def add(a: int, b: int):
